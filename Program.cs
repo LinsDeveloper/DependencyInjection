5ec0b2972607c0c7f2fa3d7efae2773e1cccb27a
Pedro Lins <pedrolins42@gmail.com>
2023-12-20 23:47:13 -0300

    feat: Resolvendo dependência AddTransient

--- Program.cs
+++ Program.cs
@@ -1,9 +1,12 @@
+using DependencyInjection.Repositories;
+using DependencyInjection.Repositories.Contracts;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
+builder.Services.AddTransient<ICustomerRepository, CustomerRepository>();
+builder.Services.AddTransient<IPromoCodeRepository, PromoCodeRepository>();
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
